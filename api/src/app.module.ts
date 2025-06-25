13c50e124b4d9bd4e3d16a66f7391e909045842d
Sehbaz Rafik <sehbazm@gmail.com>
2025-06-25 23:35:01 +0100

    refactor: main and app api files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './modules/auth/auth.module';
 import { ProjectModule } from './modules/project/project.module';
 import { TaskModule } from './modules/task/task.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, ProjectModule, TaskModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    ProjectModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/main.ts
+++ api/src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configureNestJsTypebox } from 'nestjs-typebox';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-async function bootstrap() {
+
+const bootstrap = async (): Promise<void> => {
+  configureNestJsTypebox();
+
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
@@ -18,8 +22,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  configureNestJsTypebox();
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+};
 
-  await app.listen(process.env.PORT ?? 3000);
-}
 bootstrap();
